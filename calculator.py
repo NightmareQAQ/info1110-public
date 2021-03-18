f93482fc766468459b092ff6a213f904df57121a
NightmareQAQ <78647571+NightmareQAQ@users.noreply.github.com>
2021-03-18 08:05:11 +0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num_1 = float(input("first number:"))
+op=input("opeartor")
+num_2 = float(input("second number"))
+
+if op == "+":
+	print(num_1+num_2)
+elif op == "-":
+	print(num_1-num_2)
+elif op == "*":
+	print(num_1*num_2)
+elif  op == "/":
+	print(num_1/num_2)
+else:
+	print("fuck you")
+
